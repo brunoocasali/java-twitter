ca3e66e353da9c690ba79d2a16cce2f5b4fa56ab
Bruno Casali <brunoocasali@gmail.com>
2014-11-18 23:41:08 -0200

    Twitter has been working very well in Application Console

---
+++ src/br/edu/unisep/models/TwitterApplication.java
@@ -0,0 +1,32 @@
+package br.edu.unisep.models;
+
+import java.util.List;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class TwitterApplication {
+	public static void main(String[] args) {
+		Twitter twitter = new TwitterFactory().getInstance();
+		try {
+			Query query = new Query("#justin");
+			QueryResult result;
+			do {
+				result = twitter.search(query);
+				List<Status> tweets = result.getTweets();
+				for (Status tweet : tweets) {
+					System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+				}
+			} while ((query = result.nextQuery()) != null);
+			System.exit(0);
+		} catch (TwitterException te) {
+			te.printStackTrace();
+			System.out.println("Failed to search tweets: " + te.getMessage());
+			System.exit(-1);
+		}
+	}
+}
